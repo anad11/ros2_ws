ee0199e423306b6ccb4fa36422361bbd3af4987b
Ana <anadimitrievska17@gmail.com>
2025-11-10 01:25:56 +0000

    trying with cpp getting controller issues

--- src/moveit_cpp_scripts/src/moveit_plan.cpp
+++ src/moveit_cpp_scripts/src/moveit_plan.cpp
@@ -5,40 +5,40 @@
 int main(int argc, char** argv)
 {
     rclcpp::init(argc, argv);
-    auto node = rclcpp::Node::make_shared("moveit_incremental_plan");
+    auto node = rclcpp::Node::make_shared("go_to_start");
 
     // Planning group name for Tiago
-    const std::string planning_group = "arm_left_torso";
+    static const std::string PLANNING_GROUP = "arm_left_torso";
 
-    moveit::planning_interface::MoveGroupInterface move_group(node, planning_group);
+    moveit::planning_interface::MoveGroupInterface move_group(node, PLANNING_GROUP);
 
-    // Always start from the current robot state
+
+    std :: vector <double> start_pose = {
+        1.5781,   // arm_left_1_joint
+       -0.5320,   // arm_left_2_joint
+       -2.3198,   // arm_left_3_joint
+       -1.4679,   // arm_left_4_joint
+       -1.1135,   // arm_left_5_joint
+        1.7486,   // arm_left_6_joint
+        0.6406    // arm_left_7_joint
+    };
+
+
     move_group.setStartStateToCurrentState();
-
-    // Get current pose
-    geometry_msgs::msg::Pose current_pose = move_group.getCurrentPose().pose;
-
-    // Example: incrementally move +10cm in x, +5cm in z
-    geometry_msgs::msg::Pose target_pose = current_pose;
-    target_pose.position.x += 0.1;
-    target_pose.position.z += 0.05;
-
-    move_group.setPoseTarget(target_pose);
-
+    move_group.setJointValueTarget(start_pose);
     moveit::planning_interface::MoveGroupInterface::Plan plan;
-
-    // Plan from current state
     bool success = (move_group.plan(plan) == moveit::core::MoveItErrorCode::SUCCESS);
-
     if (success)
     {
-        RCLCPP_INFO(node->get_logger(), "Plan successful, executing...");
+        RCLCPP_INFO(node->get_logger(), "Plan to start pose successful, executing...");
         move_group.execute(plan);
     }
     else
     {
-        RCLCPP_WARN(node->get_logger(), "Planning failed!");
+        RCLCPP_WARN(node->get_logger(), "Planning to start pose failed!");
+        return 1;
     }
+
 
     rclcpp::shutdown();
     return 0;
